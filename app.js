7e39bd1dcdc21d1ee14d6e405d9214115ab5a0e9
carlhauck <carlhauck@gmail.com>
2020-09-21 18:04:13 -0500

    completes assignment with both css and js files served

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+const guestRoutes = require('./routes/guest');
+const { dirname } = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(guestRoutes);
+
+app.listen(3000);
